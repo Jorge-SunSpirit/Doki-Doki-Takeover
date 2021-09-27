683a7e7b5e21540a114ad413d6992c9a92de7094
M&M <goldtextwitch@outlook.com>
2021-09-26 17:02:03 -0700

    remove polymod

--- source/Note.hx
+++ source/Note.hx
@@ -6,9 +6,6 @@ import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
-#if polymod
-import polymod.format.ParseRules.TargetSignatureElement;
-#end
 import PlayState;
 
 using StringTools;
