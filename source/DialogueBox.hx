4edaa5b89dbc590ed632494e837cc7507dcc0c30
M&M <goldtextwitch@outlook.com>
2021-10-18 16:28:15 -0700

    making sure that non-sys platforms can still compile

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -1176,10 +1176,14 @@ class DialogueBox extends FlxSpriteGroup
 	{
 		var splitName:Array<String> = dialogueList[0].split(":");
 		curCharacter = splitName[1];
+		#if FEATURE_FILESYSTEM
 		if (CoolUtil.isRecording())
 			dialogueList[0] = StringTools.replace(dialogueList[0].substr(splitName[1].length + 2).trim(), '{USERNAME}', Sys.environment()["USERNAME"]);
 		else
 			dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+		#else
+		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+		#end
 	}
 
 	function funnyGlitch():Void
