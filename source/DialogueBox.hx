dc7fbb8da270b6a8df8b446fb13f3eeca6f3673f
M&M <goldtextwitch@outlook.com>
2021-10-18 16:19:04 -0700

    actually make sure this doesn't get replaced outside of epiphany lol oops

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -1176,11 +1176,10 @@ class DialogueBox extends FlxSpriteGroup
 	{
 		var splitName:Array<String> = dialogueList[0].split(":");
 		curCharacter = splitName[1];
-		#if FEATURE_FILESYSTEM
-		dialogueList[0] = StringTools.replace(dialogueList[0].substr(splitName[1].length + 2).trim(), '{USERNAME}', Sys.environment()["USERNAME"]);
-		#else
-		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
-		#end
+		if (CoolUtil.isRecording())
+			dialogueList[0] = StringTools.replace(dialogueList[0].substr(splitName[1].length + 2).trim(), '{USERNAME}', Sys.environment()["USERNAME"]);
+		else
+			dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
 	}
 
 	function funnyGlitch():Void
